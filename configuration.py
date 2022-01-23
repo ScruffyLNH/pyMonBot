ac280171727b27595963889ad5a6d45d3a0c82b6
Ndnes <>
2022-01-23 04:43:16 +0100

    Added superUsers to config

--- configuration.py
+++ configuration.py
@@ -1,9 +1,11 @@
 from pydantic import BaseModel
 from decimal import Decimal
+from typing import List
 
 class Configuration(BaseModel):
 
     LfServerId: int = None # Id of the Legacy Fleet server
     adminId: int = 312381318891700224 # Scruffy_90
+    superUsers: List[int] = None
     contributerVoiceCategoryName: str = None # The category from wich to pull participants from all VCs.
     serviceCharge: Decimal = 0.005 # Service charge for transactions in the MO.Trader
