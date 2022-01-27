1e1d64cd63220eadb70bc6cad700a3ed7c959db6
Ndnes <>
2022-01-27 01:01:10 +0100

    Added new project constants to Constants class.

--- constants.py
+++ constants.py
@@ -12,4 +12,13 @@ class Constants:
     CONFIG_DATA_FILENAME = 'config.json'
 
     # Name of the leger (main accounting) file
-    LEDGER_FILENAME = "ledger.json"
+    LEDGER_FILENAME = 'ledger.json'
+
+    # URL link to embed thumbnail.
+    THUMBNAIL_URL = (
+        'https://cdn.discordapp.com/attachments/'
+        '935142356557131816/935142474110865448/LF_emblem_lo_res.png'
+    )
+
+    # Color used for bot theme. Embeds roles etc.
+    BOT_COLOR = 0xFFCC00
