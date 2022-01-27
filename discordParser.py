60b5518640cfc67cad5d77afe4bd747367237e6f
Ndnes <>
2022-01-27 01:03:14 +0100

    Add discord parser module.

---
+++ discordParser.py
@@ -0,0 +1,44 @@
+import discord # noqa
+    
+async def getVoiceUsers():
+    pass
+
+def getVoiceChannelsByCategoryName(ctx, name):
+    
+    categories = ctx.guild.categories
+    categoryChannel = None
+    for c in categories:
+        if c.name.upper() == name.upper():
+            categoryChannel = c
+            break
+    
+    if categoryChannel:
+        return categoryChannel.voice_channels
+    else:
+        return None
+
+async def getTrackedVoiceChannels():
+    pass
+
+def getShareholders(
+    bot,
+    ctx,
+    excludeBots=False,
+    excludeAuthor=False,
+):
+    
+    voiceChannels = getVoiceChannelsByCategoryName(
+        ctx,
+        bot.config.contributerVoiceCategoryName,
+    )
+
+    shareHolders = []
+    for vc in voiceChannels:
+        shareHolders += vc.members
+
+    # Exclude any bots and the invoker of the command.
+    shareHolders = list(
+        filter(lambda s: not(s.bot or s.id == ctx.author.id), shareHolders)
+    )
+
+    return shareHolders
