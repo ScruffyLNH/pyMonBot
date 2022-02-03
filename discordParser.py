233f040ab10e688cba5145547e147d7c96ae2815
Ndnes <>
2022-02-03 01:13:57 +0100

    Add methods to discordParser.

--- discordParser.py
+++ discordParser.py
@@ -1,7 +1,44 @@
-import discord # noqa
+from decimal import Decimal, InvalidOperation
     
 async def getVoiceUsers():
     pass
+
+def getSuperUserMentions(bot, ctx):
+    mentions = ''
+    for u in ctx.guild.members:
+        if u.id in bot.config.superUsers:
+            mentions += u.mention
+    return mentions
+
+def parseUsersWithShares(ctx):
+    """Parses roles of the format <value><x> in to a dictionary.
+    value is used for the key and the role's members as the dictionary value
+
+    Args:
+        ctx Class: discord.ext.commands.Context: Context from a discord command.
+
+    Returns:
+        Dict: Dictionary with share value as key and role id and value.
+    """
+
+    roles = ctx.guild.roles
+
+    users = {}
+    foundValues = []
+    for role in roles:
+        nameSplit = role.name.split('x')
+        if len(nameSplit) == 2 and nameSplit[1] == '':
+            try:
+                value = Decimal(nameSplit[0])
+                if value not in foundValues:
+                    users[value] = role.members
+                    foundValues.append(value)
+            except InvalidOperation:
+                pass
+    return users
+
+    # Return a dictionary with value and id 
+    # i.e role= 1.2x return [{1.2(decimal):ID(int)}]
 
 def getVoiceChannelsByCategoryName(ctx, name):
     
